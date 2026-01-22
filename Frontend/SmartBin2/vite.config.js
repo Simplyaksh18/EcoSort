219bd558b28e81f9dd123317ddb9a1e6f55d35e1
Simplyaksh18 <akshieta@gmail.com>
2026-01-22 19:22:34 +0530

    fix: correct base path for Cloudflare Pages

--- Frontend/SmartBin2/vite.config.js
+++ Frontend/SmartBin2/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/EcoSort/", // MUST match repo name exactly
+  base: "/", // MUST match repo name exactly
 
   plugins: [
     react(),
